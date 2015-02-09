ca8fc7f3f6e0427977e2f00f2c6aaffdb609a710
Sangwoo <newksw121@gmail.com>
2015-02-09 06:29:09 +0000

    Add html5shiv and respond.js

--- app/views/includes/head.blade.php
+++ app/views/includes/head.blade.php
@@ -7,7 +7,12 @@
 <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet">
 {{ HTML::style('css/app.css'); }}
 {{ HTML::style('css/main.css'); }}
-
+<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
+  <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+<![endif]-->
 
 <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
 <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
