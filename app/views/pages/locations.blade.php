43ed33557a56f63e365e0a3bc2ebcb9298f7caa9
Sangwoo <newksw121@gmail.com>
2015-02-14 09:46:42 +0000

    add js files and correct location

--- app/views/pages/locations.blade.php
+++ app/views/pages/locations.blade.php
@@ -2,32 +2,6 @@
 @section('content')
 <div class="container">
   <p>{{ Lang::get("location.address") }}, {{ Lang::get("location.tel") }}</p>
-  <table cellpadding="0" cellspacing="0"> 
-    <tbody>
-      <tr> 
-        <td style="border:1px solid #cecece;">
-          <a href="http://map.naver.com/?dlevel=11&amp;lat=37.4988427&amp;lng=127.0383503&amp;searchCoord=126.9854855%3B37.4797723&amp;query=7ISc7Jq47IucIOqwleuCqOq1rCDsl63sgrwy64%2BZIDcyNy0xNCDtj4ntmZTruYzrlKk%3D&amp;menu=location&amp;tab=1&amp;mapMode=0&amp;enc=b64" target="_blank">
-            <img class="img-responsive" src="http://prt.map.naver.com/mashupmap/print?key=p1361455536501_1063864687"   alt="ì§ë í¬ê² ë³´ê¸°" title="ì§ë í¬ê² ë³´ê¸°" border="0" style="vertical-align:top;">
-          </a>
-        </td> 
-      </tr> 
-      <tr> 
-        <td>  
-          <table cellpadding="0" cellspacing="0" width="100%">  
-            <tbody>
-              <tr>  
-                <td height="30" bgcolor="#f9f9f9" align="left" style="padding-left:9px; border-left:1px solid #cecece; border-bottom:1px solid #cecece;">   
-                  <span style="font-family: tahoma; font-size: 11px; color:#666;">2013.2.21</span>&nbsp;<span style="font-size: 11px; color:#e5e5e5;">|</span>&nbsp;<a style="font-family: dotum,sans-serif; font-size: 11px; color:#666; text-decoration: none; letter-spacing: -1px;" href="http://map.naver.com/?dlevel=11&amp;lat=37.4988427&amp;lng=127.0383503&amp;searchCoord=126.9854855%3B37.4797723&amp;query=7ISc7Jq47IucIOqwleuCqOq1rCDsl63sgrwy64%2BZIDcyNy0xNCDtj4ntmZTruYzrlKk%3D&amp;menu=location&amp;tab=1&amp;mapMode=0&amp;enc=b64" target="_blank">ì§ë í¬ê² ë³´ê¸°</a>  
-                </td>  
-                <td width="98" bgcolor="#f9f9f9" align="right" style="text-align:right; padding-right:9px; border-right:1px solid #cecece; border-bottom:1px solid #cecece;">   
-                  <span style="float:right;"><span style="font-size:9px; font-family:Verdana, sans-serif; color:#444;">©&nbsp;</span>&nbsp;<a style="font-family:tahoma; font-size:9px; font-weight:bold; color:#009bc8; text-decoration:none;" href="http://www.nhncorp.com" target="_blank">NHN Corp.</a></span>  
-                </td>  
-              </tr>  
-            </tbody>
-          </table> 
-        </td> 
-      </tr>  
-    </tbody>
-  </table>
+  <table cellpadding="0" cellspacing="0"> <tr> <td style="border:1px solid #cecece;"><a href="http://map.naver.com/?menu=location&mapMode=0&lat=37.4988708&lng=127.0294918&dlevel=12&searchCoord=126.9768001%3B37.5320002&query=7ISc7Jq47IucIOqwleuCqOq1rCDthYztl6TrnoDroZwgN%2Bq4uCAxMSjsl63sgrzrj5kg7ZWc642V67mM65SpKSAzMDLtmLg%3D&mpx=09170625%3A37.5320002%2C126.9768001%3AZ11%3A0.0344611%2C0.0174057&tab=1&enc=b64" target="_blank"><img src="http://prt.map.naver.com/mashupmap/print?key=p1423907156918_2030933667" width="534" height="418" alt="지도 크게 보기" title="지도 크게 보기" border="0" style="vertical-align:top;"/></a></td> </tr> <tr> <td>  <table cellpadding="0" cellspacing="0" width="100%">  <tr>  <td height="30" bgcolor="#f9f9f9" align="left" style="padding-left:9px; border-left:1px solid #cecece; border-bottom:1px solid #cecece;">   <span style="font-family: tahoma; font-size: 11px; color:#666;">2015.2.14</span>&nbsp;<span style="font-size: 11px; color:#e5e5e5;">|</span>&nbsp;<a style="font-family: dotum,sans-serif; font-size: 11px; color:#666; text-decoration: none; letter-spacing: -1px;" href="http://map.naver.com/?menu=location&mapMode=0&lat=37.4988708&lng=127.0294918&dlevel=12&searchCoord=126.9768001%3B37.5320002&query=7ISc7Jq47IucIOqwleuCqOq1rCDthYztl6TrnoDroZwgN%2Bq4uCAxMSjsl63sgrzrj5kg7ZWc642V67mM65SpKSAzMDLtmLg%3D&mpx=09170625%3A37.5320002%2C126.9768001%3AZ11%3A0.0344611%2C0.0174057&tab=1&enc=b64" target="_blank">지도 크게 보기</a>  </td>  <td width="98" bgcolor="#f9f9f9" align="right" style="text-align:right; padding-right:9px; border-right:1px solid #cecece; border-bottom:1px solid #cecece;">   <span style="float:right;"><span style="font-size:9px; font-family:Verdana, sans-serif; color:#444;">&copy;&nbsp;</span>&nbsp;<a style="font-family:tahoma; font-size:9px; font-weight:bold; color:#2db400; text-decoration:none;" href="http://www.nhncorp.com" target="_blank">NAVER Corp.</a></span>  </td>  </tr>  </table> </td> </tr>  </table>
 </div>
 @stop
